705a521b5d13e143a66caa56b49e8ec92e14f0e7
Sercan S <83725191+ragiii@users.noreply.github.com>
2022-05-31 22:58:42 +0300

    Update main.java

--- main.java
+++ main.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args){
         double kilo,boy,kitle;
-        Scanner giridi=new Scanner(System.in);
-        System.out.print("Lütfen boyunuzu metre cinsinden giriniz :");
-        boy=giridi.nextDouble();
-        System.out.print("Lütfen kilonusu giriniz : ");
-        kilo=giridi.nextDouble();
+        Scanner input=new Scanner(System.in);
+        System.out.print("Lütfen boyunuzu (metre cinsinden) giriniz :");
+        boy=input.nextDouble();
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo=input.nextDouble();
         kitle = kilo / (boy * boy) ;
         System.out.print("Vücut kitel indexniz :" + kitle);
 
